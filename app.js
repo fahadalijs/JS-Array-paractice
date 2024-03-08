cfe5e41c04e156c7dae4ba703e56bd6944412e8d
fahadalijs <fahad9827022@gmail.com>
2024-03-08 18:06:09 +0500

    JS Array paractice

---
+++ app.js
@@ -0,0 +1,259 @@
+// console.log(Array);
+
+
+// let
+// const
+
+
+
+// var
+// redeclear
+// reassign
+
+
+
+
+
+
+// let userName = "Fahad";
+// let userName = "Ammar"
+// userName = "Ali"
+// console.log(userName);
+// // let hammy reassign krny  deta ha
+// // let hammy redeclear krny nahi deta
+
+
+
+// const userName = "Fahad"
+// const userName = "Ali"
+// console.log(userName)
+// // const hammy reassign krny  or redeclear krny nhi deta ha
+
+
+
+
+// let cities = ["Karachi" , "Islamabad" , "Faislabad" , "Gilgit" , "Hydrabad" , "sheikarpur" ,['fb area' , 'liyari' , 'golimar']];
+// console.log(cities);
+// console.log(cities.length);
+// console.log(cities [5]);
+// console.log(cities[5][2]);
+// console.log(cities)
+// console.log(typeof(cities))
+
+
+
+// let cities = ["Karachi" , "Islamabad" , "Lahore" , "Faislabad" , "Hydrabad"];
+// console.log(cities)
+// console.log(cities.length)
+// console.log(cities [0])
+// console.log(typeof(cities))
+
+
+
+
+
+
+
+
+// let cities = ["Karachi" , "Lahore" , "Islamabad" , "Faislabad" , "Hyderabad"];
+// console.log(cities);
+// console.log(cities.length);
+// console.log(cities[4]);
+// console.log(typeof(cities));
+
+
+// const user = ["Fahad" , "Ali" , "Balti"];
+// console.log(`${user[0]}`,  `${user[1]}` , `${user[2]}`);
+
+
+
+// const user = ["fahad" , "Ali" , "Balti"];
+// console.log(`${user[0]}` , `${user[1]}` , `${user[2]}`);
+
+
+
+
+
+
+// const user = ["Fahad" , "Ali" , "Balti"];
+// console.log(`${user[0]}` , `${user[1]}` , `${user[2]}`);
+
+
+
+
+// user[0] = "Fahad";
+// user[4] = "Ali";
+// console.log(user[4]);
+
+
+
+
+// const user = [];
+// user[0] = prompt("Enter name 1");
+// user[1] = prompt("Enter name 2");
+// user[2] = prompt("Enter name 3");
+// user[3] = prompt("Enter name 4");
+// console.log(user);
+
+
+
+
+// const user = [];
+// user[0] = prompt("Enter your first name"); 
+// user[1] = prompt("Enter your second name"); 
+// user[2] = prompt("Enter your third name") ;
+// user[3] = prompt("Enter your fourth name") ;
+// console.log(user);
+
+
+
+
+
+
+// const user = [];
+// user[0] = prompt("Enter your first name");
+// user[1] = prompt("Enter your second name");
+// user[2] = prompt("Enter your third name");
+// user[3] = prompt("Enter your fourth name");
+// console.log(user);
+
+
+
+
+
+// const user = [];
+// user[0] = prompt("Enter your first name");
+// user[1] = prompt("Enter your second name");
+// user[2] = prompt("Enter your third name");
+// user[3] = prompt("Enter your fourth name");
+// console.log(user);
+
+
+
+
+
+
+// const user = [];
+// user[0] = prompt("Enter your first name")
+// user[2] = prompt("Enter your second name")
+// user[3] = prompt("Enter your third name")
+// user[4] = prompt("Enter your fourth name")
+// console.log(user)
+
+
+
+
+
+
+
+
+// let fruit = ["Mango" , "Bananna" , "Orange" , "Graps"]
+// console.log(fruit)
+
+
+
+
+// Arry methods
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+// const fruit = ["Mango" , "Bananna" , "Orange" , "Graps"];
+// fruit.push("Watermelon");
+// fruit.pop();
+// fruit.unshift("watermelon")
+// fruit.shift()
+// console.log(fruit);
+
+
+
+// fruits.splice('shurw kaha sa krna ha' , 'kitna delete krna hain' , 'uski jaga kiya rkhna haa')
+
+// const fruit = ['c];
+// fruit.splice(0 , 5);
+// console.log(fruit);
+
+
+
+
+// const fruit = ['strawberry' , 'watermelon' , 'orange' , 'mangoes' , 'guava' , 'nashpati' , 'grapes' , 'anaaar' , 'kinno']
+// fruit.splice(3 , 5)
+// console.log(fruit)
+
+
+// const fruit = ['strawberry' , 'watermelon' , 'orange' , 'mangoes' , 'guava' , 'nashpati' , 'grapes' , 'anaaar' , 'kinno']
+// fruit.splice(1 , 2 , "Fahad")
+// console.log(fruit)
+
+
+
+
+
+
+// slice is not understanding
+
+
+
+
+
+
+// const names = ["Fahad" , "Ali" , "Balti"];
+// const str = names.join(" ")
+// console.log(str)
+
+
+
+
+
+// const names = ["Fahad" , "Ali" , "Balti"];
+// const join = names.join(" ");
+// console.log(join);
+
+
+
+
+// const names = ["Fahad" , "Ali" , "Balti"];
+// const join = names.join(" ")
+// console.log(join)
+
+
+
+
+
+
+// const names = ("Fahad Ali Balti");
+// const split = names.split(" ");
+// console.log(split);
+
+
+
+
+
+
+
+
+
+// const names = ("Fahad Ali Balti");
+// const split = names.split(" ");
+// console.log(split);
+
+
+
+
+
+
+
+
+
+
+;
